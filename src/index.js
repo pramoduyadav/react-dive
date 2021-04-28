9aa90abb3303aaa8704f5796273e2c779699a105
pramoduyadav <pramoduyadav@gmail.com>
2021-04-28 22:15:02 +0530

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "./playground/redux-101.js";
+//import App from "./App";
 
 class ControlApp extends React.Component {
   handleAlert() {
